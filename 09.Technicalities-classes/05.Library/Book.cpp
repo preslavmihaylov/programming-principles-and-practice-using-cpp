8dfbd1ef24fe09cdccebe77a56d2bee63347b17b
unknown <pmihaylov95@gmail.com>
2016-08-13 11:46:54 +0300

    Completed the library tasks

--- 09.Technicalities-classes/05.Library/Book.cpp
+++ 09.Technicalities-classes/05.Library/Book.cpp
@@ -1,17 +1,107 @@
 #include "Book.h"
+#include <stdexcept>
+#include <regex>
+#include <iostream>
 
 using namespace std;
 
-bool IsValidISBN(string ISBN)
+bool IsDigit(char ch)
 {
-
+    return (ch >= '0' && ch <= '9');
 }
 
-Book::Book(string author, string title, string ISBN)
+bool IsAlphabeticChar(char ch)
 {
+    return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
+}
+
+bool IsValidISBN(const string & ISBN)
+{
+    int dashesCnt = 0;
+    bool valueFound = false;
+
+    for (int chIndex = 0; chIndex < ISBN.size(); ++chIndex)
+    {
+        // Is dash
+        if (ISBN[chIndex] == '-')
+        {
+            if (valueFound == false)
+            {
+                return false;
+            }
+
+            ++dashesCnt;
+            valueFound = false;
+        }
+        else if (dashesCnt < 3)
+        {
+            if (!IsDigit(ISBN[chIndex]))
+            {
+                return false;
+            }
+
+            valueFound = true;
+        }
+        else if (dashesCnt == 3)
+        {
+            if (!IsDigit(ISBN[chIndex]) && !IsAlphabeticChar(ISBN[chIndex]))
+            {
+                return false;
+            }
+
+            valueFound = true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    if (dashesCnt != 3)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool operator==(const Book& bk1, const Book& bk2)
+{
+    return bk1.GetISBN() == bk2.GetISBN();
+}
+
+bool operator!=(const Book& bk1, const Book& bk2)
+{
+    return !(bk1 == bk2);
+}
+
+std::ostream& operator<<(std::ostream& os, const Book& bk)
+{
+    os << bk.GetTitle() << " - " << bk.GetISBN();
+    return os;
+}
+
+Book::Book(string author, string title, string ISBN, double price)
+    : Book(author, title, ISBN, price, Book::fiction)
+{
+}
+
+
+Book::Book(std::string author, std::string title, std::string ISBN, double price, Genre genre)
+{
+    if (!IsValidISBN(ISBN))
+    {
+        throw runtime_error("Invalid ISBN");
+    }
+
     this->author = author;
     this->title = title;
     this->ISBN = ISBN;
     time_t now = time(0);
     this->copyrightDate = *localtime(&now);
+    this->price = price;
+    this->genre = genre;
+    this->isCheckedOut = false;
 }
--- 09.Technicalities-classes/05.Library/Book.h
+++ 09.Technicalities-classes/05.Library/Book.h
@@ -3,22 +3,38 @@
 
 #include <ctime>
 #include <string>
+#include <ostream>
 
 class Book
 {
 public:
-    Book(std::string author, std::string title, std::string ISBN);
+    enum Genre { fiction, nonfiction, periodical, biography, children };
 
-    std::string GetTitle() { return this->title; };
-    std::string GetAuthor() { return this->author; };
-    std::string GetISBN() { return this->ISBN; };
-    tm GetCopyrightDate() { return this->copyrightDate; };
+    Book(std::string author, std::string title, std::string ISBN, double price);
+    Book(std::string author, std::string title, std::string ISBN, double price, Genre genre);
+
+    void CheckOut() { this->isCheckedOut = true; };
+
+    Genre GetGenre() const { return this->genre; };
+    std::string GetTitle() const { return this->title; };
+    std::string GetAuthor() const { return this->author; };
+    std::string GetISBN() const { return this->ISBN; };
+    double GetPrice() const { return this->price; };
+    bool IsCheckedOut() const { return this->isCheckedOut; };
+    tm GetCopyrightDate() const { return this->copyrightDate; };
 
 private:
+    Genre genre;
     std::string title;
     std::string author;
     std::string ISBN;
+    double price;
+    bool isCheckedOut;
     tm copyrightDate;
 };
+
+bool operator==(const Book& bk1, const Book& bk2);
+bool operator!=(const Book& bk1, const Book& bk2);
+std::ostream& operator<<(std::ostream& os, const Book& bk);
 
 #endif // BOOK_H
