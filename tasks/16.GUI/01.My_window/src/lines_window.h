2bb0f3f3d07258bde9baac358b5ac1599ead4bd3
Preslav Mihaylov <pmihaylov95@gmail.com>
2016-12-18 21:34:12 +0200

    removed all exe files from the repo

---
+++ tasks/16.GUI/01.My_window/src/lines_window.h
@@ -0,0 +1,33 @@
+#include "GUI.h"
+#include "Graph.h"
+
+struct Lines_window : public Graph_lib::Window
+{
+	Lines_window(Point xy, int w, int h, const string& title);
+private:
+	Graph_lib::Out_box current_xy;
+	Graph_lib::In_box x_next;
+	Graph_lib::In_box y_next;
+	Graph_lib::Button next_button;
+	Graph_lib::Button quit_button;
+	Graph_lib::Button color_menu_button;
+	Graph_lib::Menu color_menu; 
+
+	Graph_lib::Open_polyline lines;
+
+	static void cb_color_menu_show(Graph_lib::Address, Graph_lib::Address pw);
+	void color_menu_show();
+
+	static void cb_red(Graph_lib::Address, Graph_lib::Address pw);
+	void red();
+	static void cb_blue(Graph_lib::Address, Graph_lib::Address pw);
+	void blue();
+	static void cb_black(Graph_lib::Address, Graph_lib::Address pw);
+	void black();
+	
+	static void cb_next(Graph_lib::Address, Graph_lib::Address pw);
+	void next();
+	static void cb_quit(Graph_lib::Address, Graph_lib::Address pw);
+	void quit();
+};
+
